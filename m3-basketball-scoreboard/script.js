b2c1921382388e5aeb546873ee93fbf8764d286e
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-18 12:53:34 +0800

    Add New Game button

--- m3-basketball-scoreboard/script.js
+++ m3-basketball-scoreboard/script.js
@@ -3,6 +3,7 @@ const guestScoreEl = document.querySelector('#guest-score');
 
 const homeAddScoreBtns = document.querySelectorAll('.team__btn.home-btn');
 const guestAddScoreBtns = document.querySelectorAll('.team__btn.guest-btn');
+const newGameBtn = document.querySelector('.new-game-btn');
 
 let homeScore = 0;
 let guestScore = 0;
@@ -19,4 +20,11 @@ guestAddScoreBtns.forEach((btn, i) => {
         guestScore += i + 1;
         guestScoreEl.textContent = guestScore;
     });
+});
+
+newGameBtn.addEventListener('click', () => {
+    homeScore = 0;
+    homeScoreEl.textContent = 0;
+    guestScore = 0;
+    guestScoreEl.textContent = 0;
 });
