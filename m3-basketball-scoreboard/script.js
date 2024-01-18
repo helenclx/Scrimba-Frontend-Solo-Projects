9b2161217a4db100b92413bc520e3cbeda46704c
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-18 12:21:51 +0800

    Refactor score buttons

--- m3-basketball-scoreboard/script.js
+++ m3-basketball-scoreboard/script.js
@@ -1,13 +1,8 @@
 const homeScoreEl = document.querySelector('#home-score');
 const guestScoreEl = document.querySelector('#guest-score');
 
-const homeAddOneBtn = document.querySelector('#home-add-1');
-const homeAddTwoBtn = document.querySelector('#home-add-2');
-const homeAddThreeBtn = document.querySelector('#home-add-3');
-
-const guestAddOneBtn = document.querySelector('#guest-add-1');
-const guestAddTwoBtn = document.querySelector('#guest-add-2');
-const guestAddThreeBtn = document.querySelector('#guest-add-3');
+const homeAddScoreBtns = document.querySelectorAll('.team__btn.home-btn');
+const guestAddScoreBtns = document.querySelectorAll('.team__btn.guest-btn');
 
 let homeScore = 0;
 let guestScore = 0;
