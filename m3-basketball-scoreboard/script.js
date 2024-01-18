1b741e4772a94f69688a20899cc091c0e274f905
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-18 12:14:53 +0800

    Set up JavaScript

---
+++ m3-basketball-scoreboard/script.js
@@ -0,0 +1,13 @@
+const homeScoreEl = document.querySelector('#home-score');
+const guestScoreEl = document.querySelector('#guest-score');
+
+const homeAddOneBtn = document.querySelector('#home-add-1');
+const homeAddTwoBtn = document.querySelector('#home-add-2');
+const homeAddThreeBtn = document.querySelector('#home-add-3');
+
+const guestAddOneBtn = document.querySelector('#guest-add-1');
+const guestAddTwoBtn = document.querySelector('#guest-add-2');
+const guestAddThreeBtn = document.querySelector('#guest-add-3');
+
+let homeScore = 0;
+let guestScore = 0;
