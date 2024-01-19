8ec3c7cbc1ba83d4a424ee3688f98536a345d337
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-19 21:25:48 +0800

    Remove unneeded function

--- m3-password-generator/script.js
+++ m3-password-generator/script.js
@@ -1,6 +1,7 @@
 const alphas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
 const alphasStr = alphas.join("");
 const numbersStr = numbers.join("");
 const symbolsStr = symbols.join("");
@@ -32,11 +33,6 @@ const getRandomPassword = (length, characters) => {
         );
     }
     return randomPassword;
-};
-
-const getRandomCharacter = () => {
-    const randomChar = Math.floor(Math.random() * characters.length);
-    return characters[randomChar];
 };
 
 clipboardBtns.forEach((btn, i) => {
