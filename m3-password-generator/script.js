2a79a9695a02d7610acc34d943f3c4007283a0f8
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-19 21:19:23 +0800

    Add functions for password settings

--- m3-password-generator/script.js
+++ m3-password-generator/script.js
@@ -1,4 +1,10 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+const alphas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+const alphasStr = alphas.join("");
+const numbersStr = numbers.join("");
+const symbolsStr = symbols.join("");
+
 let passwordLength = 15;
 
 const passwordBtn = document.querySelector(".password-btn");
@@ -11,14 +17,19 @@ const numberCheckbox = document.querySelector("#password-numbers");
 const symbolCheckbox = document.querySelector("#password-symbols");
 
 passwordBtn.addEventListener("click", () => {
-    passwordOneEl.textContent = getRandomPassword();
-    passwordTwoEl.textContent = getRandomPassword();
+    let characters = alphasStr;
+    numberCheckbox.checked ? (characters += numbersStr) : "";
+    symbolCheckbox.checked ? (characters += symbolsStr) : "";
+    passwordOneEl.textContent = getRandomPassword(passwordLength, characters);
+    passwordTwoEl.textContent = getRandomPassword(passwordLength, characters);
 });
 
-const getRandomPassword = () => {
+const getRandomPassword = (length, characters) => {
     let randomPassword= "";
-    for (let i = 0; i < passwordLength; i++) {
-        randomPassword += getRandomCharacter();
+    for (let i = 0; i < length; i++) {
+        randomPassword += characters.charAt(
+            Math.floor(Math.random() * characters.length)
+        );
     }
     return randomPassword;
 };
