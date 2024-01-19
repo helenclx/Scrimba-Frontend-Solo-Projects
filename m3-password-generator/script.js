05ceceaf397e868a37588abcf1288e62e3a9cbed
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-01-19 16:26:34 +0800

    Set up password elements

--- m3-password-generator/script.js
+++ m3-password-generator/script.js
@@ -1,3 +1,5 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let passwordLength = 15;
+const passwordOneEl = document.querySelector("#password-1");
+const passwordTwoEl = document.querySelector("#password-2");
