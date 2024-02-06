fa20385c7fe62a190b9327f378563039a62d1253
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-02-06 14:01:21 +0800

    Use JavaScript to render all posts

--- m4-oldagram/script.js
+++ m4-oldagram/script.js
@@ -28,3 +28,28 @@ const posts = [
     }
 ]
 
+const postContainerEl = document.querySelector(".post-container");
+
+posts.forEach((post) => {
+    postContainerEl.innerHTML += `
+        <article class="post">
+            <section class="post__author">
+                <img src="${post.avatar}" alt="Avatar of ${post.name}" class="post__author--avatar">
+                <div class="post__author--info">
+                    <p class="post__author--name">${post.name}</p>
+                    <p class="post__author--location">${post.location}</p>
+                </div>
+            </section>
+            <img src="${post.post}" alt="Illustration of ${post.name}" class="post__img">
+            <section class="post__content">
+                <div class="post__content--icons">
+                    <img src="images/icon-heart.png" alt="">
+                    <img src="images/icon-comment.png" alt="">
+                    <img src="images/icon-dm.png" alt="">
+                </div>
+                <p class="post__content-likes">${post.likes} likes</p>
+                <p class="post__content--caption"><span class="post__content--username">${post.username}</span> ${post.comment}</p>
+            </section>
+        </article>
+    `
+});
