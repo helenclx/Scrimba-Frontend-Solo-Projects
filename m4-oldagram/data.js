cf4304f1a1283ba09bf229a76cac1328bcc845e3
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-02-18 21:09:26 +0800

    Add post ID and isLiked boolean

--- m4-oldagram/data.js
+++ m4-oldagram/data.js
@@ -1,29 +1,35 @@
 export const posts = [
     {
+        id:'b5782b80-66f7-48a0-ad99-28cc7abd0358',
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false
     },
     {
+        id: 'dceeb080-ec52-4943-ba8a-1a010e38a08b',
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false
     },
-        {
+    {
+        id: 'f723bef0-641c-4f16-a68a-ca828ca562f0',
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false
     }
 ]
