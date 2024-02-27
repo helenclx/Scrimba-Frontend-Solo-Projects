900bd42817aaff3f7d5deffcef44ebeda7757be2
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-02-27 16:05:24 +0800

    Set variables for DOM elements

--- m5-restaurant-ordering-app/script.js
+++ m5-restaurant-ordering-app/script.js
@@ -1,1 +1,11 @@
 import menuArray from './data.js'
+
+const menuEl = document.querySelector('.menu');
+const orderEl = document.querySelector('.order');
+const totalPriceEl = document.querySelector('.order__total--price');
+const orderSubmitBtn = document.querySelector('.order__submit-btn');
+const modealEl = document.querySelector('.modal');
+const modalFormEl = document.querySelector('.modal__form');
+const cardNameInput = document.getElementById('card-name-input');
+const cardNumInput = document.getElementById('card-number-input');
+const cardCvvInput = document.getElementById('card-cvv-input');
