d594f5b4d5d99560b11a2e64408c2a098d58e2dc
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-03-28 17:18:07 +0800

    Display color scheme results

--- m8-color-scheme-generator/script.js
+++ m8-color-scheme-generator/script.js
@@ -3,6 +3,7 @@
 const formEl = document.querySelector('.form');
 const seedColorEl = document.querySelector('.form__seed-color');
 const colorScemeEl = document.querySelector('.form__color-scheme');
+const resultsContainer = document.querySelector('.results');
 
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -13,6 +14,25 @@ formEl.addEventListener('submit', (event) => {
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColorEl.value.slice(1)}&mode=${colorScemeEl.value}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            resultsContainer.innerHTML = '';
+
+            data.colors.forEach(color => {
+                const colorHexValue = color.hex.value;
+
+                const newResult = document.createElement('div');
+                newResult.classList.add('result');
+
+                const newResultColor = document.createElement('div');
+                newResultColor.classList.add('result__color');
+                newResultColor.style.backgroundColor = colorHexValue;
+
+                const newResultValue = document.createElement('p');
+                newResultValue.classList.add('result__value');
+                newResultValue.textContent = colorHexValue;
+
+                resultsContainer.append(newResult);
+                newResult.append(newResultColor);
+                newResult.append(newResultValue);
+            })
         })
 })
