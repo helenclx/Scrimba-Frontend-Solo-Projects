20c1b6f573d34acc87b780ce03432a185efec953
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-03-28 14:43:25 +0800

    Set up form submission functionality

--- m8-color-scheme-generator/script.js
+++ m8-color-scheme-generator/script.js
@@ -1,1 +1,8 @@
 // API documentation: https://www.thecolorapi.com/docs#schemes
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Form submitted');
+})
