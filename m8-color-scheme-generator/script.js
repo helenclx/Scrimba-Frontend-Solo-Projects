da58c257dffd616e5ae1144480542a525c03462e
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-03-28 14:47:52 +0800

    Get input of seed color and color scheme

--- m8-color-scheme-generator/script.js
+++ m8-color-scheme-generator/script.js
@@ -1,8 +1,12 @@
 // API documentation: https://www.thecolorapi.com/docs#schemes
 
 const formEl = document.querySelector('.form');
+const seedColorEl = document.querySelector('.form__seed-color');
+const colorScemeEl = document.querySelector('.form__color-scheme');
 
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('Form submitted');
+    console.log('Seed Color:', seedColorEl.value);
+    console.log('Color Scheme:', colorScemeEl.value);
 })
