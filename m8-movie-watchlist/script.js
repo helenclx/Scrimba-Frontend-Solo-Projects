48201ad491e4c790a29cd486f2121ca143ea5e13
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-02 17:09:28 +0800

    Set up functionality to display search results

--- m8-movie-watchlist/script.js
+++ m8-movie-watchlist/script.js
@@ -12,12 +12,41 @@ searchForm.addEventListener('submit', (e) => {
 });
 
 const searchMovies = async (input) => {
-    input = input.replace(/\s+/gi, '+');
+    const paramStr = input.replace(/\s+/gi, '+');
 
     try {
-        const response = await fetch(`https://www.omdbapi.com/?s=${input}&type=movie&apikey=${API_KEY}`);
+        const response = await fetch(`https://www.omdbapi.com/?s=${paramStr}&type=movie&apikey=${API_KEY}`);
         const data = await response.json();
-        console.log(data);
+        console.log("Search Results:", data);
+
+        resultsContainer.innerHTML = `<h2>Search results for "${input}":</h2>`
+
+        data.Search.forEach(result => {
+            displayMovieResult(result.imdbID);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const displayMovieResult = async (imdbID) => {
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`);
+        const data = await response.json();
+
+        const {Poster, Title, imdbRating, Runtime, Genre, Plot} = data;
+
+        resultsContainer.innerHTML += `
+            <div class="result">
+                <img src="${Poster}" alt="Poster of ${Title}">
+                <h3>${Title}</h3>
+                <p>${imdbRating}</p>
+                <p>${Runtime}</p>
+                <p>${Genre}</p>
+                <button class="result__watchlist-btn" data-watchlist="${imdbID}" aria-label="Add to watchlist">Watchlist</button>
+                <p>${Plot}</p>
+            </div>
+        `;
     } catch (error) {
         console.error(error);
     }
