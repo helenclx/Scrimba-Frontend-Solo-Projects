f94f4b71c6ddcce0a454ad0c7a075f731d38cd6c
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-02 15:40:32 +0800

    Initiate variables of DOM elements

--- m8-movie-watchlist/script.js
+++ m8-movie-watchlist/script.js
@@ -1,2 +1,7 @@
 // OMDb API: https://www.omdbapi.com/
 const API_KEY = '65605e18';
+
+const searchForm = document.querySelector('.search');
+const movieSearchInput = document.getElementById('movie-search');
+const resultsContainer = document.querySelector('.results');
+const watchlistContainer = document.querySelector('.watchlist');
