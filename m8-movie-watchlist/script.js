9fda83721e76beb01b0a720f0966fb96cb2528ac
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-02 16:09:19 +0800

    Set up movie search function

--- m8-movie-watchlist/script.js
+++ m8-movie-watchlist/script.js
@@ -8,4 +8,17 @@ const watchlistContainer = document.querySelector('.watchlist');
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    searchMovies(movieSearchInput.value);
 });
+
+const searchMovies = async (input) => {
+    input = input.replace(/\s+/gi, '+');
+
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?s=${input}&type=movie&apikey=${API_KEY}`);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
