740c3ba4f733f29db9646c3368a6cd6b09679b4a
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-04 16:25:49 +0800

    Add scroll to top button

--- m8-movie-watchlist/script.js
+++ m8-movie-watchlist/script.js
@@ -148,3 +148,21 @@ const displayWatchlist = () => {
 if (watchlistContainer) {
     displayWatchlist();
 }
+
+const topButton = document.querySelector(".top-btn");
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.classList.remove("hidden");
+    } else {
+        topButton.classList.add("hidden");
+    }
+}
+
+topButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
